ed978f05be11c3a0ffcb23274dad94003877947c
Arunkumar45678 <sikha.arun@gmail.com>
2026-01-27 19:14:07 +0530

    Update auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-  static const String baseUrl = "https://YOUR_PROJECT_ID.supabase.co";
-  static const String anonKey = "YOUR_PUBLIC_ANON_KEY";
+  static const String baseUrl = "https://diomisfrhfinohloejyx.supabase.co";
+  static const String anonKey = "sb_publishable_r1xOPpLNLfmmZH8L7TaRKQ_4dABy1c4";
 
   static Future<Map<String, dynamic>> login(String username, String password) async {
     final res = await http.post(
