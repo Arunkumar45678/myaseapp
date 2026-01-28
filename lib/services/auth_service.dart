1d7abf12ca4ef27d0ed928e96dcb480a256e8366
Arunkumar45678 <sikha.arun@gmail.com>
2026-01-28 21:20:52 +0530

    Refactor login method to use Supabase client

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,23 +1,24 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthService {
-  static const String baseUrl = "https://diomisfrhfinohloejyx.supabase.co";
-  static const String anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpb21pc2ZyaGZpbm9obG9lanl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MzQxNzMsImV4cCI6MjA4NTAxMDE3M30.y1cFnb0TSLdiaZAAiOMJgmoLXgj4WSMvP5vO7SIZiOE";
+  static final SupabaseClient _supabase =
+      Supabase.instance.client;
 
-  static Future<Map<String, dynamic>> login(String username, String password) async {
-    final res = await http.post(
-      Uri.parse("$baseUrl/rest/v1/rpc/login_user"),
-      headers: {
-        "apikey": anonKey,
-        "Content-Type": "application/json"
+  static Future<Map<String, dynamic>> login(
+      String username, String password) async {
+
+    final response = await _supabase.rpc(
+      'login_user',
+      params: {
+        'p_username': username,
+        'p_password': password,
       },
-      body: jsonEncode({
-        "p_username": username,
-        "p_password": password
-      }),
     );
 
-    return jsonDecode(res.body);
+    if (response is Map<String, dynamic>) {
+      return response;
+    } else {
+      throw Exception("Invalid response from server");
+    }
   }
 }
