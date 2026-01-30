e0fd2226ce960f19a19d4739f94fe6ad83f0d7ed
Arunkumar45678 <sikha.arun@gmail.com>
2026-01-30 16:11:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ Future<void> main() async {
 
   await Supabase.initialize(
     url: 'https://diomisfrhfinohloejyx.supabase.co',
-    anonKey: 'PASTE_YOUR_ANON_KEY',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpb21pc2ZyaGZpbm9obG9lanl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MzQxNzMsImV4cCI6MjA4NTAxMDE3M30.y1cFnb0TSLdiaZAAiOMJgmoLXgj4WSMvP5vO7SIZiOE',
   );
 
   runApp(const MyAseApp());
