9f0dff0f40f39cd1b06d420f3b26a7c5515c42ce
Arunkumar45678 <sikha.arun@gmail.com>
2026-01-30 15:23:24 +0530

    Update session_service.dart

--- lib/services/session_service.dart
+++ lib/services/session_service.dart
@@ -1,17 +1,18 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SessionService {
-  static final _storage = FlutterSecureStorage();
+  static const _storage = FlutterSecureStorage();
+  static const _key = "user_session";
 
   static Future<void> saveUser(String userId) async {
-    await _storage.write(key: "user_id", value: userId);
+    await _storage.write(key: _key, value: userId);
   }
 
   static Future<String?> getUser() async {
-    return await _storage.read(key: "user_id");
+    return await _storage.read(key: _key);
   }
 
-  static Future<void> logout() async {
-    await _storage.deleteAll();
+  static Future<void> clear() async {
+    await _storage.delete(key: _key);
   }
 }
